f2da9c45d8f3aadbd3d55603b271635a98bf13ff
nehabht <ne.bhatti2@icloud.com>
2022-03-24 15:23:18 +0100

    add ticket price

---
+++ assets/js/media.js
@@ -0,0 +1,34 @@
+// chiedere all'utente il numero di chilometri che vuole percorrere
+
+const km_da_fare = parseInt(prompt("quanti chilomentri vuoi fare?"))
+
+// l'età del passeggero
+
+const anni_utente = parseInt(prompt("quanti anni hai?"))
+
+// prezzo del biglietto 0.21 euro per km
+
+const prezzo_biglietto = 0.21 * km_da_fare
+
+
+// calcolare prezzo scontato del 20% per i minorenni e 40% per over 
+
+if (anni_utente < 18){
+    var prezzo_minorenni = prezzo_biglietto - (prezzo_biglietto * 20 / 100);  
+} else if (anni_utente > 64){
+    var prezzo_over65 = prezzo_biglietto - (prezzo_biglietto * 40 / 100);
+} else {
+    console.log(`Il prezzo del biglietto è ${prezzo_biglietto} €`)
+}
+
+// definire e mostrare prezzo minorenni e over65 con 2 cifre decimali
+
+if (prezzo_minorenni){
+    console.log(`Il prezzo del biglietto scontanto per minorenni è ${prezzo_minorenni.toFixed(2)} €`);
+} else{
+    console.log(`Il prezzo del biglietto scontato per over 65 è ${prezzo_over65.toFixed(2)} €`)
+}
+
+// comunicare il prezzo del biglietto
+
+
